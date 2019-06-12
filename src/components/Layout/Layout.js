ca701cb90d99d320691ab7047a79dae271b71b72
Sandesh Tiwari <sandeshtiwari@live.com>
2019-06-11 23:14:00 -0500

    Initial commit to save

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Aux from '../../hoc/Auxillary';
+import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
+const layout = (props) => (
+    <Aux>
+        <Toolbar />
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
